5b26a4b6b5380af3d98c1b68f8f9c2e4a8778cab
태현 <78199616+NekoNyangYee@users.noreply.github.com>
2025-08-31 20:48:04 +0900

    add: ci.yml 수정 24

--- models.py
+++ models.py
@@ -11,13 +11,13 @@ class MsgPayload(BaseModel):
     msg_name: str
 
 
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(255), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
+
 
 class Login(Base):
     __tablename__ = "logins"
